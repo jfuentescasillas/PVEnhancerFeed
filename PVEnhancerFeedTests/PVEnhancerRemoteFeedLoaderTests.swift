c8ed23d709351e39a5b8ff84f5fee6701a06cfc4
jfuentescasillas <j.fuentes.casillas@gmail.com>
2024-04-23 17:20:47 +0200

    Improve readability with helper method

--- PVEnhancerFeedTests/PVEnhancerRemoteFeedLoaderTests.swift
+++ PVEnhancerFeedTests/PVEnhancerRemoteFeedLoaderTests.swift
@@ -44,7 +44,7 @@ final class PVEnhancerRemoteFeedLoaderTests: XCTestCase {
         sut.load { capturedErrors.append($0) }
         
         let clientError = NSError(domain: "Test", code: 0)
-        client.completions[0](clientError)
+        client.complete(with: clientError)
         
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
@@ -67,6 +67,11 @@ final class PVEnhancerRemoteFeedLoaderTests: XCTestCase {
         func get(from url: URL, completion: @escaping (Error) -> Void) {
             completions.append(completion)
             requestedURLs.append(url)
+        }
+        
+        
+        func complete(with error: Error, at index: Int = 0) {
+            completions[index](error)
         }
     }
 }
