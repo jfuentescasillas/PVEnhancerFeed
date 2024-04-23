a88ce22f8c80a64b03c44d6fc4ccee4723fbf7fe
jfuentescasillas <j.fuentes.casillas@gmail.com>
2024-04-23 17:49:42 +0200

    Remove default value for completion block (moved it to the test)

--- IrradianceFeedAPI/RemoteFeedLoader.swift
+++ IrradianceFeedAPI/RemoteFeedLoader.swift
@@ -32,7 +32,7 @@ public class RemoteFeedLoader {
     }
     
     
-    public func load(completion: @escaping (Error) -> Void = { _ in }) {
+    public func load(completion: @escaping (Error) -> Void) {
         client.get(from: url) { error in
             completion(.connectivity)
         }
