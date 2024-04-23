b0ebd7dd7a191273381176e3161a91de9c52edaf
jfuentescasillas <j.fuentes.casillas@gmail.com>
2024-04-23 12:41:20 +0200

    -RemoteFeedLoader does not request data upon creation -Created test_load_requestDataFromURL() -Place test logic in HTTPClientSpy -Created HTTPClientProtocol -Preventing  duplication by giving an URL in the url parameter in the makeSUT() method

--- PVEnhancerFeedTests/PVEnhancerFeedTests.swift
+++ PVEnhancerFeedTests/PVEnhancerFeedTests.swift
@@ -9,21 +9,63 @@
 import XCTest
 
 
+// MARK: - RemoteFeedLoader Class
+class RemoteFeedLoader {
+    let url: URL
+    let client: HTTPClientProtocol
+        
+    
+    init(url: URL, client: HTTPClientProtocol) {
+        self.url = url
+        self.client = client
+    }
+    
+    
+    func load() {
+        client.get(from: url)
+    }
+}
+
+
+// MARK: - HTTPClient Protocol
+protocol HTTPClientProtocol {
+    func get(from url: URL)
+}
+
+
+// MARK: - PVEnhancerFeedTests Class
 final class PVEnhancerFeedTests: XCTestCase {
-    
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_init_doesNotRequestDataFromURL() {
+        let (_, client) = makeSUT()
+        
+        XCTAssertNil(client.requestedURL)
     }
     
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURL, url)
     }
     
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    // MARK: - Helpers
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        
+        return (sut, client)
+    }
+    
+    
+    private class HTTPClientSpy: HTTPClientProtocol {
+        var requestedURL: URL?
+        
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
     }
 }
