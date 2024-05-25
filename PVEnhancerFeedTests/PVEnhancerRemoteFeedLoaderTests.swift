858988c2d0568436a5e713f540a2a6fcb406fdaa
jfuentescasillas <j.fuentes.casillas@gmail.com>
2024-05-25 12:41:51 -0600

    forgot this update for previous commit

--- PVEnhancerFeedTests/PVEnhancerRemoteFeedLoaderTests.swift
+++ PVEnhancerFeedTests/PVEnhancerRemoteFeedLoaderTests.swift
@@ -209,7 +209,7 @@ final class PVEnhancerRemoteFeedLoaderTests: XCTestCase {
         }
         
         
-        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
             let response = HTTPURLResponse(
                 url: requestedURLs[index],
                 statusCode: code,
