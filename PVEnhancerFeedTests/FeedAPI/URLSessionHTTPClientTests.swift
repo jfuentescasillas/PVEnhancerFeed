a0ed28cd1e444135f7cfcd3e923559a6fa084212
jfuentescasillas <j.fuentes.casillas@gmail.com>
2024-06-04 10:51:25 +0200

    Refactor URLSession subclass mocking with protocol based mocking

--- PVEnhancerFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ PVEnhancerFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -10,11 +10,21 @@ import XCTest
 import PVEnhancerFeed
 
 
+protocol HTTPSessionProtocol {
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> HTTPSessionTask
+}
+
+
+protocol HTTPSessionTask {
+    func resume()
+}
+
+
 class URLSessionHTTPClient {
-    private let session: URLSession
+    private let session: HTTPSessionProtocol
     
     
-    init(session: URLSession) {
+    init(session: HTTPSessionProtocol) {
         self.session = session
     }
     
@@ -32,12 +42,11 @@ class URLSessionHTTPClient {
 class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_resumesDataTaskWithURL() {
         let url = URL(string: "http://any-url.com")!
-        let session = URLSessionSpy()
+        let session = HTTPSessionSpy()
         let task = URLSessionDataTaskSpy()
         session.stub(url: url, task: task)
         
         let sut = URLSessionHTTPClient(session: session)
-        
         sut.get(from: url) { _ in }
         
         XCTAssertEqual(task.resumeCallCount, 1)
@@ -47,16 +56,16 @@ class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_failsOnRequestError() {
         let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
-        let session = URLSessionSpy()
+        let session = HTTPSessionSpy()
         session.stub(url: url, error: error)
         
-        let sut = URLSessionHTTPClient(session: session)
         let exp = expectation(description: "Wait for completion")
-        
+        let sut = URLSessionHTTPClient(session: session)
         sut.get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError, error)
+           
             default:
                 XCTFail("Expected failure with error \(error), got \(result) instead")
             }
@@ -69,21 +78,22 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     
     // MARK: - Helpers
-    private class URLSessionSpy: URLSession {
+    private class HTTPSessionSpy: HTTPSessionProtocol {
         private var stubs = [URL: Stub]()
         
+        
         private struct Stub {
-            let task: URLSessionDataTask
+            let task: HTTPSessionTask
             let error: Error?
         }
         
         
-        func stub(url: URL, task: URLSessionDataTask = FakeURLSessionDataTask(), error: Error? = nil) {
+        func stub(url: URL, task: HTTPSessionTask = FakeURLSessionDataTask(), error: Error? = nil) {
             stubs[url] = Stub(task: task, error: error)
         }
         
         
-        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> HTTPSessionTask {
             guard let stub = stubs[url] else {
                 fatalError("Couln't find stub for \(url)")
             }
@@ -95,16 +105,16 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     
-    private class FakeURLSessionDataTask: URLSessionDataTask {
-        override func resume() {}
+    private class FakeURLSessionDataTask: HTTPSessionTask {
+        func resume() {}
     }
     
     
-    private class URLSessionDataTaskSpy: URLSessionDataTask {
+    private class URLSessionDataTaskSpy: HTTPSessionTask {
         var resumeCallCount = 0
         
         
-        override func resume() {
+        func resume() {
             resumeCallCount += 1
         }
     }
