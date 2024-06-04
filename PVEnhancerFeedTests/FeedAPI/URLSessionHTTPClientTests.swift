3ccee0e0f89ef1c987a149b7f8d4cd9c01320883
jfuentescasillas <j.fuentes.casillas@gmail.com>
2024-06-04 18:36:12 +0200

    Move the URLSessionHTTPClient (the system under test, or "SUT") creation to a factory method to protect our tests from breaking changes

--- PVEnhancerFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ PVEnhancerFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -13,9 +13,11 @@ import PVEnhancerFeed
 class URLSessionHTTPClient {
     private let session: URLSession
     
+    
     init(session: URLSession = .shared) {
         self.session = session
     }
+    
     
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
         session.dataTask(with: url) { _, _, error in
@@ -52,7 +54,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
     }
@@ -64,10 +66,9 @@ class URLSessionHTTPClientTests: XCTestCase {
         
         URLProtocolStub.stub(data: nil, response: nil, error: error)
         
-        let sut = URLSessionHTTPClient()
         let exp = expectation(description: "Wait for completion")
         
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertNotNil(receivedError)
@@ -84,6 +85,11 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     
     // MARK: - Helpers
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
+    
+    
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
         private static var requestObserver: ((URLRequest) -> Void)?
