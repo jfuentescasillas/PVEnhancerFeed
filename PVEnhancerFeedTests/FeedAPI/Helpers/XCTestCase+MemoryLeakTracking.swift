26be4d493d6c56f32965d809fde67d8cc8f90590
jfuentescasillas <j.fuentes.casillas@gmail.com>
2024-06-04 19:51:01 +0200

    Extract memory leak tracking helper method to a shared extension on XCTestCase

---
+++ PVEnhancerFeedTests/FeedAPI/Helpers/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,18 @@
+//
+//  XCTestCase+MemoryLeakTracking.swift
+//  PVEnhancerFeedTests
+//
+//  Created by jfuentescasillas on 04/06/2024.
+//
+
+
+import XCTest
+
+
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+}
--- PVEnhancerFeedTests/FeedAPI/PVEnhancerRemoteFeedLoaderTests.swift
+++ PVEnhancerFeedTests/FeedAPI/PVEnhancerRemoteFeedLoaderTests.swift
@@ -174,13 +174,6 @@ final class PVEnhancerRemoteFeedLoaderTests: XCTestCase {
     }
     
     
-    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
-        }
-    }
-    
-    
     private func makeItem(coordinates: [Double], dni: [String: Double], ghi: [String: Double], dhi: [String: Double]) -> (model: IrradiancesFeed, json: [String: Any]) {
         let geometry = Geometry(coordinates: coordinates)
         let parameter = Parameter(allskySfcSwDni: dni, allskySfcSwDwn: ghi, allskySfcSwDiff: dhi)
