a968923e2577ffc2ea15c378dce48af3507e24f0
jfuentescasillas <j.fuentes.casillas@gmail.com>
2024-06-13 12:43:22 +0200

    Load command delivers no images on more than seven days old cache

--- PVEnhancerFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
+++ PVEnhancerFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
@@ -67,6 +67,17 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
     }
     
     
+    func test_load_deliversNoIrradiancesOnMoreThanSevenDaysOldCache() {
+        let fixedCurrentDate = Date()
+        let moreThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: -1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        
+        expect(sut, toCompleteWith: .success(IrradiancesFeed(geometry: .empty, properties: .empty)), when: {
+            store.completeRetrieval(with: uniqueIrradiancesFeed().local, timestamp: moreThanSevenDaysOldTimestamp)
+        })
+    }
+    
+    
     // MARK: - Helpers
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
         let store = FeedStoreSpy()
