fcd2ddb9a0585b08166bda04d38f77fd6d9d5ecc
jfuentescasillas <j.fuentes.casillas@gmail.com>
2024-07-01 14:20:00 +0200

    LocalFeedLoader in integration with the CoreDataFeedStore delivers items saved on separate instances, proving we correctly persist the data models to disk.

--- PVEnhancerFeedCacheIntegrationTests/PVEnhancerFeedCacheIntegrationTests.swift
+++ PVEnhancerFeedCacheIntegrationTests/PVEnhancerFeedCacheIntegrationTests.swift
@@ -51,6 +51,39 @@ final class PVEnhancerFeedCacheIntegrationTests: XCTestCase {
     }
         
     
+    func test_load_deliversItemsSavedOnASeparateInstance() {
+        let sutToPerformSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
+        let feed = uniqueIrradiancesFeed().model
+        
+        let saveExp = expectation(description: "Wait for save completion")
+        sutToPerformSave.save(feed) { saveError in
+            XCTAssertNil(saveError, "Expected to save feed successfully")
+            saveExp.fulfill()
+        }
+        
+        wait(for: [saveExp], timeout: 1)
+        
+        let loadExp = expectation(description: "Wait for load completion")
+        sutToPerformLoad.load { loadResult in
+            switch loadResult {
+            case let .success(irradianceFeed):
+                XCTAssertEqual(irradianceFeed, feed)
+                
+            case let .failure(error):
+                XCTFail("Expected successful feed result, got \(error) instead")
+                
+            @unknown default:
+                XCTFail("Received an unknown result")
+            }
+            
+            loadExp.fulfill()
+        }
+        
+        wait(for: [loadExp], timeout: 1)
+    }
+    
+    
     // MARK: Helpers
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> LocalFeedLoader {
         let storeBundle = Bundle(for: CoreDataFeedStore.self)
