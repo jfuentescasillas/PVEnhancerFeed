82ef267a07ea63d06eea5f6f445dd7d9f42fd66a
jfuentescasillas <j.fuentes.casillas@gmail.com>
2024-07-01 13:25:07 +0200

    Add PVEnhancerFeedCacheIntegrationTests target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ PVEnhancerFeedCacheIntegrationTests/PVEnhancerFeedCacheIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  PVEnhancerFeedCacheIntegrationTests.swift
+//  PVEnhancerFeedCacheIntegrationTests
+//
+//  Created by jfuentescasillas on 01/07/2024.
+//
+
+
+import XCTest
+
+
+final class PVEnhancerFeedCacheIntegrationTests: XCTestCase {
+    
+}
