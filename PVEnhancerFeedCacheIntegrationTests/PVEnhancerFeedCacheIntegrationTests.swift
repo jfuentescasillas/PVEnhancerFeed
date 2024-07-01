514806d37c4737d57a3d7aea35dbaa07a86cdd6f
jfuentescasillas <j.fuentes.casillas@gmail.com>
2024-07-01 13:51:20 +0200

    -LocalFeedLoader in integration with the CoreDataFeedStore delivers no items on empty cache -Make "public" some parameters in the IRradiancesFeed file

--- PVEnhancerFeedCacheIntegrationTests/PVEnhancerFeedCacheIntegrationTests.swift
+++ PVEnhancerFeedCacheIntegrationTests/PVEnhancerFeedCacheIntegrationTests.swift
@@ -7,8 +7,56 @@
 
 
 import XCTest
+import PVEnhancerFeed
 
 
 final class PVEnhancerFeedCacheIntegrationTests: XCTestCase {
+    func test_load_deliversNoItemsOnEmptyCache() {
+        let sut = makeSUT()
+        
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result {
+            case let .success(irradiancesFeed):
+                XCTAssertTrue(irradiancesFeed.geometry.coordinates?.isEmpty ?? true, "Expected empty coordinates")
+                XCTAssertTrue(irradiancesFeed.properties.parameter?.allskySfcSwDni?.isEmpty ?? true, "Expected empty allskySfcSwDni")
+                XCTAssertTrue(irradiancesFeed.properties.parameter?.allskySfcSwDwn?.isEmpty ?? true, "Expected empty allskySfcSwDwn")
+                XCTAssertTrue(irradiancesFeed.properties.parameter?.allskySfcSwDiff?.isEmpty ?? true, "Expected empty allskySfcSwDiff")
+
+            case let .failure(error):
+                XCTFail("Expected successful feed result, got \(error) instead")
+                        
+            @unknown default:
+                XCTFail("Received an unknown result")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1)
+    }
+        
     
+    // MARK: Helpers
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> LocalFeedLoader {
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let storeURL = testSpecificStoreURL()
+        let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+        let sut = LocalFeedLoader(store: store, currentDate: Date.init)
+        
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return sut
+    }
+    
+    
+    private func testSpecificStoreURL() -> URL {
+        return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    
+    private func cachesDirectory() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
 }
