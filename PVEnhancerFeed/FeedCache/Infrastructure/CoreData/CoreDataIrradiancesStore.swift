400be3851cf23c18e519d1931b7f2ba5331dc799
jfuentescasillas <j.fuentes.casillas@gmail.com>
2024-07-02 13:34:18 +0200

    Simplify CoreDataFeedStore completion code with the new Result APIs: retrieve does not use do-try-catch block

--- PVEnhancerFeed/FeedCache/Infrastructure/CoreData/CoreDataIrradiancesStore.swift
+++ PVEnhancerFeed/FeedCache/Infrastructure/CoreData/CoreDataIrradiancesStore.swift
@@ -22,44 +22,35 @@ public final class CoreDataIrradiancesStore: FeedStoreProtocol {
     
     // MARK: - Methods related to Retrieve data
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        performRetrieval { result in
-                completion(result)
+        perform { context in
+            guard let cache = try? ManagedCache.find(in: context),
+                  let managedFeed = cache.irradiancesFeed.firstObject as? ManagedIrradiancesFeed 
+            else {
+                completion(.success(.none))
+            
+                return
             }
-    }
-    
-    
-    private func performRetrieval(completion: @escaping (Result<CachedFeed?, Error>) -> Void) {
-        perform { (context: NSManagedObjectContext) in
-            do {
-                if let cache = try ManagedCache.find(in: context),
-                   let managedFeed = cache.irradiancesFeed.firstObject as? ManagedIrradiancesFeed {
-                    
-                    // Convert coordinates string to array of doubles
-                    let coordinates = managedFeed.geometry?.coordinates?.components(separatedBy: ",").compactMap { Double($0.trimmingCharacters(in: .whitespaces)) } ?? []
-                    
-                    // Retrieve parameters from ManagedParameter and convert to dictionaries
-                    let dniDict = managedFeed.properties?.parameter?.allskySfcSwDni?.toDictionary()
-                    let ghiDict = managedFeed.properties?.parameter?.allskySfcSwDwn?.toDictionary()
-                    let dhiDict = managedFeed.properties?.parameter?.allskySfcSwDiff?.toDictionary()
-                    
-                    let parameter = Parameter(
-                        allskySfcSwDni: dniDict,
-                        allskySfcSwDwn: ghiDict,
-                        allskySfcSwDiff: dhiDict
-                    )
-                    
-                    let localFeed = LocalIrradiancesFeed(
-                        geometry: Geometry(coordinates: coordinates),
-                        properties: Properties(parameter: parameter)
-                    )
-                    
-                    completion(.success(CachedFeed(feed: localFeed, timestamp: cache.timestamp)))
-                } else {
-                    completion(.success(nil))
-                }
-            } catch {
-                completion(.failure(error))
-            }
+
+            // Convert coordinates string to array of doubles
+            let coordinates = managedFeed.geometry?.coordinates?.components(separatedBy: ",").compactMap { Double($0.trimmingCharacters(in: .whitespaces)) } ?? []
+            
+            // Retrieve parameters from ManagedParameter and convert to dictionaries
+            let dniDict = managedFeed.properties?.parameter?.allskySfcSwDni?.toDictionary()
+            let ghiDict = managedFeed.properties?.parameter?.allskySfcSwDwn?.toDictionary()
+            let dhiDict = managedFeed.properties?.parameter?.allskySfcSwDiff?.toDictionary()
+            
+            let parameter = Parameter(
+                allskySfcSwDni: dniDict,
+                allskySfcSwDwn: ghiDict,
+                allskySfcSwDiff: dhiDict
+            )
+            
+            let localFeed = LocalIrradiancesFeed(
+                geometry: Geometry(coordinates: coordinates),
+                properties: Properties(parameter: parameter)
+            )
+
+            completion(.success(CachedFeed(feed: localFeed, timestamp: cache.timestamp)))
         }
     }
     
