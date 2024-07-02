a8ac56fe7fead4675355f766958296c01ff3f2ef
jfuentescasillas <j.fuentes.casillas@gmail.com>
2024-07-02 14:08:12 +0200

    Simplify CoreDataFeedStore completion code with the new Result APIs: retrieve, insert and deleteCachedFeed do not use do-try-catch block at all

--- PVEnhancerFeed/FeedCache/Infrastructure/CoreData/CoreDataIrradiancesStore.swift
+++ PVEnhancerFeed/FeedCache/Infrastructure/CoreData/CoreDataIrradiancesStore.swift
@@ -18,39 +18,38 @@ public final class CoreDataIrradiancesStore: FeedStoreProtocol {
         container = try NSPersistentContainer.load(modelName: "IrradiancesFeedStore", url: storeURL, in: bundle)
         context = container.newBackgroundContext()
     }
-
+    
     
     // MARK: - Methods related to Retrieve data
     public func retrieve(completion: @escaping RetrievalCompletion) {
         perform { context in
-            guard let cache = try? ManagedCache.find(in: context),
-                  let managedFeed = cache.irradiancesFeed.firstObject as? ManagedIrradiancesFeed 
-            else {
-                completion(.success(.none))
-            
-                return
-            }
-
-            // Convert coordinates string to array of doubles
-            let coordinates = managedFeed.geometry?.coordinates?.components(separatedBy: ",").compactMap { Double($0.trimmingCharacters(in: .whitespaces)) } ?? []
-            
-            // Retrieve parameters from ManagedParameter and convert to dictionaries
-            let dniDict = managedFeed.properties?.parameter?.allskySfcSwDni?.toDictionary()
-            let ghiDict = managedFeed.properties?.parameter?.allskySfcSwDwn?.toDictionary()
-            let dhiDict = managedFeed.properties?.parameter?.allskySfcSwDiff?.toDictionary()
-            
-            let parameter = Parameter(
-                allskySfcSwDni: dniDict,
-                allskySfcSwDwn: ghiDict,
-                allskySfcSwDiff: dhiDict
-            )
-            
-            let localFeed = LocalIrradiancesFeed(
-                geometry: Geometry(coordinates: coordinates),
-                properties: Properties(parameter: parameter)
-            )
-
-            completion(.success(CachedFeed(feed: localFeed, timestamp: cache.timestamp)))
+            completion(Result {
+                guard let cache = try ManagedCache.find(in: context),
+                      let managedFeed = cache.irradiancesFeed.firstObject as? ManagedIrradiancesFeed else {
+                    return nil
+                }
+                
+                // Convert coordinates string to array of doubles
+                let coordinates = managedFeed.geometry?.coordinates?.components(separatedBy: ",").compactMap { Double($0.trimmingCharacters(in: .whitespaces)) } ?? []
+                
+                // Retrieve parameters from ManagedParameter and convert to dictionaries
+                let dniDict = managedFeed.properties?.parameter?.allskySfcSwDni?.toDictionary()
+                let ghiDict = managedFeed.properties?.parameter?.allskySfcSwDwn?.toDictionary()
+                let dhiDict = managedFeed.properties?.parameter?.allskySfcSwDiff?.toDictionary()
+                
+                let parameter = Parameter(
+                    allskySfcSwDni: dniDict,
+                    allskySfcSwDwn: ghiDict,
+                    allskySfcSwDiff: dhiDict
+                )
+                
+                let localFeed = LocalIrradiancesFeed(
+                    geometry: Geometry(coordinates: coordinates),
+                    properties: Properties(parameter: parameter)
+                )
+                
+                return CachedFeed(feed: localFeed, timestamp: cache.timestamp)
+            })
         }
     }
     
@@ -58,46 +57,28 @@ public final class CoreDataIrradiancesStore: FeedStoreProtocol {
     // MARK: - Methods related to Insert data
     public func insert(_ feed: LocalIrradiancesFeed, timestamp: Date, completion: @escaping InsertionCompletion) {
         perform { context in
-            guard let managedCache = try? ManagedCache.newUniqueInstance(in: context) else {
-                completion(.failure(NSError(domain: "CoreDataError", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to create new unique instance of ManagedCache."])))
+            completion(Result {
+                let managedCache = try ManagedCache.newUniqueInstance(in: context)
+                managedCache.timestamp = timestamp
                 
-                return
-            }
-            
-            managedCache.timestamp = timestamp
-            
-            let managedFeed = ManagedIrradiancesFeed.insert(feed, in: context)
-            managedCache.irradiancesFeed = NSOrderedSet(object: managedFeed)
-            
-            do {
+                let managedFeed = ManagedIrradiancesFeed.insert(feed, in: context)
+                managedCache.irradiancesFeed = NSOrderedSet(object: managedFeed)
+                
                 try context.save()
-               
-                completion(.success(()))
-            } catch {
-                completion(.failure(error))
-            }
+            })
         }
     }
     
-
+    
     // MARK: - Methods related to Deletion of data
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         perform { context in
-            guard let cache = try? ManagedCache.find(in: context) else {
-                completion(.success(()))
-                
-                return
-            }
-            
-            context.delete(cache)
-            
-            do {
-                try context.save()
-                
-                completion(.success(()))
-            } catch {
-                completion(.failure(error))
-            }
+            completion(Result {
+                if let cache = try ManagedCache.find(in: context) {
+                    context.delete(cache)
+                    try context.save()
+                }
+            })
         }
     }
     
